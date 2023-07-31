3963b0919fb92c26d3edf679450bf01280a45aa7
mosunov.nn <colt92@inbox.ru>
2023-07-31 16:56:06 +0500

    Initial commit

---
+++ src/main/java/com/example/adsonline/AdsOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.adsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdsOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/adsonline/AdsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
