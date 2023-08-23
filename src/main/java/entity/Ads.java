fa64fe481de98cc1445edf7a85d9043235bb4bc5
Kristina Adigamova <kriwowak@yandex.ru>
2023-08-24 01:20:42 +0500

    исправляю

--- src/main/java/entity/Ads.java
+++ src/main/java/entity/Ads.java
@@ -2,6 +2,7 @@ package entity;
 
 import jakarta.persistence.*;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
